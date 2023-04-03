400da04620e8c7af376be92c97203a78e32a9db1
Anisa Zahra <anisafatimahazzahra@gmail.com>
2023-04-03 12:41:48 +0700

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Anisa Fatimah Azzahra on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
